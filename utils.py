b0aaad4512254d4fc5067ebcabe228286d29e87e
Joyce Ndichu <joyce.ndichu@mpharma.com>
2024-11-27 17:48:15 +0300

    pred

--- utils.py
+++ utils.py
@@ -3,15 +3,70 @@ from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 
-# Country categories for one-hot encoding
-country_categories = ['KENYA', 'MALAWI', 'SUDAN']
+country_categories = [
+    'Algeria', 'Angola', 'Benin', 'Botswana', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cameroon', 
+    'Central African Republic', 'Chad', 'Comoros', 'Congo (Congo-Brazzaville)', 'Congo (Democratic Republic)', 
+    'Djibouti', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Eswatini', 'Ethiopia', 'Gabon', 'Gambia', 
+    'Ghana', 'Guinea', 'Guinea-Bissau', 'Ivory Coast', 'Kenya', 'Lesotho', 'Liberia', 'Libya', 'Madagascar', 
+    'Malawi', 'Mali', 'Mauritania', 'Mauritius', 'Morocco', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Rwanda', 
+    'Sao Tome and Principe', 'Senegal', 'Somalia', 'South Africa', 'South Sudan', 'Sudan', 'Tanzania', 'Togo', 'Tunisia', 
+    'Uganda', 'Zambia', 'Zimbabwe'
+]
 
-# Precomputed rainfall data (example)
 precomputed_rainfall = {
-    ("KENYA", 1): 0.1,
-    ("KENYA", 6): 0.2,
-    ("MALAWI", 1): 0.3,
-    ("SUDAN", 1): 0.6,
+    ("Kenya", 1): 0.1,
+    ("Kenya", 6): 0.2,
+    ("Malawi", 1): 0.3,
+    ("Sudan", 1): 0.6,
+    ("Algeria", 1): 0.05,  
+    ("Angola", 1): 0.1,    
+    ("Benin", 1): 0.15,   
+    ("Botswana", 1): 0.05, 
+    ("Burkina Faso", 1): 0.2,  
+    ("Burundi", 1): 0.25,  
+    ("Cabo Verde", 1): 0.1,  
+    ("Cameroon", 1): 0.3,  
+    ("Central African Republic", 1): 0.2,  
+    ("Chad", 1): 0.05,  
+    ("Comoros", 1): 0.4,  
+    ("Congo (Congo-Brazzaville)", 1): 0.3,  
+    ("Congo (Democratic Republic)", 1): 0.5,  
+    ("Djibouti", 1): 0.02,  
+    ("Egypt", 1): 0.01, 
+    ("Equatorial Guinea", 1): 0.3,  
+    ("Eritrea", 1): 0.05,  
+    ("Eswatini", 1): 0.2,  
+    ("Ethiopia", 1): 0.3,  
+    ("Gabon", 1): 0.4,  
+    ("Gambia", 1): 0.1,  
+    ("Ghana", 1): 0.2,  
+    ("Guinea", 1): 0.3,  
+    ("Guinea-Bissau", 1): 0.3,  
+    ("Ivory Coast", 1): 0.2,  
+    ("Lesotho", 1): 0.1,  
+    ("Liberia", 1): 0.3,  
+    ("Libya", 1): 0.01,  
+    ("Madagascar", 1): 0.4,  
+    ("Mali", 1): 0.05,  
+    ("Mauritania", 1): 0.02,  
+    ("Mauritius", 1): 0.3,  
+    ("Morocco", 1): 0.05,  
+    ("Mozambique", 1): 0.3,  
+    ("Namibia", 1): 0.02,  
+    ("Niger", 1): 0.05, 
+    ("Nigeria", 1): 0.2,  
+    ("Rwanda", 1): 0.3,  
+    ("Sao Tome and Principe", 1): 0.4,  
+    ("Senegal", 1): 0.25,  
+    ("Somalia", 1): 0.15,  
+    ("South Africa", 1): 0.3,  
+    ("South Sudan", 1): 0.3,  
+    ("Tanzania", 1): 0.3, 
+    ("Togo", 1): 0.2,  
+    ("Tunisia", 1): 0.05,  
+    ("Uganda", 1): 0.3,  
+    ("Zambia", 1): 0.3,  
+    ("Zimbabwe", 1): 0.3,  
 }
 
 # Month mapping
